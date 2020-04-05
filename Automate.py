3ba8463243e7518d0731f7ea227241f879167961
Mike Armistead <m.armistead91@gmail.com>
2020-04-05 15:00:09 -0400

    created a cnn model

--- Automate.py
+++ Automate.py
@@ -1,5 +1,8 @@
 import glob
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import itertools
 
 def image_counter(file):
     return len(glob.glob1(file, "*.jpeg"))
@@ -26,3 +29,58 @@ def images_to_df(filenorm, filesick):
     # Shuffle the data
     train_data = train_data.sample(frac=1.).reset_index(drop=True)
     return train_data
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion Matrix, without normalization')
+
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest',aspect='equal', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+#                  verticalalignment='center',
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+def visualize_training_results(results):
+    history = results.history
+    plt.figure()
+    plt.plot(history['val_loss'])
+    plt.plot(history['loss'])
+    plt.legend(['val_loss', 'loss'])
+    plt.title('Loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.show()
+
+    plt.figure()
+    plt.plot(history['val_accuracy'])
+    plt.plot(history['accuracy'])
+    plt.legend(['val_accuracy', 'accuracy'])
+    plt.title('Accuracy')
+    plt.xlabel('Epochs')
+    plt.ylabel('Accuracy')
+    plt.show()
